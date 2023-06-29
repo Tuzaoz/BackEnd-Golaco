a9aac7f42fe19a8147de696dc5525a5f8894dd24
Arthur Dantas <arthurdantaspro@gmail.com>
2023-06-28 22:03:48 -0300

    first commit

---
+++ src/main/java/com/devs/backendgolaco/BackEndGolacoApplication.java
@@ -0,0 +1,13 @@
+package com.devs.backendgolaco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndGolacoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndGolacoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devs/backendgolaco/BackEndGolacoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devs.backendgolaco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndGolacoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
